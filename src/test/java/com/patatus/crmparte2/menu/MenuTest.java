5d673a9fe1660191298313224a6599a5068a6e9c
Alejandro <amnaredo@gmail.com>
2021-02-04 22:35:35 +0100

    Added Menu commands tests

--- src/test/java/com/patatus/crmparte2/menu/MenuTest.java
+++ src/test/java/com/patatus/crmparte2/menu/MenuTest.java
@@ -189,5 +189,213 @@ class MenuTest {
         Assertions.assertEquals(Command.UNKNOWN, menu.checkCommand(input));
     }
 
+    @Test
+    void checkCommand_newSalesRep_NEW_SALES_REP() {
+        String input = "new salesrep";
+        Assertions.assertEquals(Command.NEW_SALESREP, menu.checkCommand(input));
+    }
+
+    @Test
+    void checkCommand_showSalesReps_SHOW_SALESREPS() {
+        String input = "show salesreps";
+        Assertions.assertEquals(Command.SHOW_SALESREPS, menu.checkCommand(input));
+    }
+
+    @Test
+    void checkCommand_reportLeadBySalesRep_REPORT_LEAD_BY_SALESREP() {
+        String input = "report lead by salesrep";
+        Assertions.assertEquals(Command.REPORT_LEAD_BY_SALESREP, menu.checkCommand(input));
+    }
+
+    @Test
+    void checkCommand_reportOpportunityBySalesRep_REPORT_OPPORTUNITY_BY_SALESREP() {
+        String input = "report opportunity by salesrep";
+        Assertions.assertEquals(Command.REPORT_OPPORTUNITY_BY_SALESREP, menu.checkCommand(input));
+    }
+
+    @Test
+    void checkCommand_reportClosedWonBySalesRep_REPORT_CLOSED_WON_BY_SALESREP() {
+        String input = "report closed-won by salesrep";
+        Assertions.assertEquals(Command.REPORT_CLOSED_WON_BY_SALESREP, menu.checkCommand(input));
+    }
+
+    @Test
+    void checkCommand_reportClosedLostBySalesRep_REPORT_CLOSED_LOST_BY_SALESREP() {
+        String input = "report closed-lost by salesrep";
+        Assertions.assertEquals(Command.REPORT_CLOSED_LOST_BY_SALESREP, menu.checkCommand(input));
+    }
+
+    @Test
+    void checkCommand_reportOpenBySalesRep_REPORT_OPEN_BY_SALESREP() {
+        String input = "report open by salesrep";
+        Assertions.assertEquals(Command.REPORT_OPEN_BY_SALESREP, menu.checkCommand(input));
+    }
+
+
+    @Test
+    void checkCommand_reportOpportunityByCountry_REPORT_OPPORTUNITY_BY_COUNTRY() {
+        String input = "report opportunity by country";
+        Assertions.assertEquals(Command.REPORT_OPPORTUNITY_BY_COUNTRY, menu.checkCommand(input));
+    }
+
+    @Test
+    void checkCommand_reportClosedWonByCountry_REPORT_CLOSED_WON_BY_COUNTRY() {
+        String input = "report closed-won by country";
+        Assertions.assertEquals(Command.REPORT_CLOSED_WON_BY_COUNTRY, menu.checkCommand(input));
+    }
+
+    @Test
+    void checkCommand_reportClosedLostByCountry_REPORT_CLOSED_LOST_BY_COUNTRY() {
+        String input = "report closed-lost by country";
+        Assertions.assertEquals(Command.REPORT_CLOSED_LOST_BY_COUNTRY, menu.checkCommand(input));
+    }
+
+    @Test
+    void checkCommand_reportOpenByCountry_REPORT_OPEN_BY_COUNTRY() {
+        String input = "report open by country";
+        Assertions.assertEquals(Command.REPORT_OPEN_BY_COUNTRY, menu.checkCommand(input));
+    }
+
+    @Test
+    void checkCommand_reportOpportunityByCity_REPORT_OPPORTUNITY_BY_CITY() {
+        String input = "report opportunity by city";
+        Assertions.assertEquals(Command.REPORT_OPPORTUNITY_BY_CITY, menu.checkCommand(input));
+    }
+
+    @Test
+    void checkCommand_reportClosedWonByCity_REPORT_CLOSED_WON_BY_CITY() {
+        String input = "report closed-won by city";
+        Assertions.assertEquals(Command.REPORT_CLOSED_WON_BY_CITY, menu.checkCommand(input));
+    }
+
+    @Test
+    void checkCommand_reportClosedLostByCity_REPORT_CLOSED_LOST_BY_CITY() {
+        String input = "report closed-lost by city";
+        Assertions.assertEquals(Command.REPORT_CLOSED_LOST_BY_CITY, menu.checkCommand(input));
+    }
+
+    @Test
+    void checkCommand_reportOpenByCity_REPORT_OPEN_BY_CITY() {
+        String input = "report open by city";
+        Assertions.assertEquals(Command.REPORT_OPEN_BY_CITY, menu.checkCommand(input));
+    }
+
+    @Test
+    void checkCommand_reportOpportunityByIndustry_REPORT_OPPORTUNITY_BY_INDUSTRY() {
+        String input = "report opportunity by industry";
+        Assertions.assertEquals(Command.REPORT_OPPORTUNITY_BY_INDUSTRY, menu.checkCommand(input));
+    }
+
+    @Test
+    void checkCommand_reportClosedWonByIndustry_REPORT_CLOSED_WON_BY_INDUSTRY() {
+        String input = "report closed-won by industry";
+        Assertions.assertEquals(Command.REPORT_CLOSED_WON_BY_INDUSTRY, menu.checkCommand(input));
+    }
+
+    @Test
+    void checkCommand_reportClosedLostByIndustry_REPORT_CLOSED_LOST_BY_INDUSTRY() {
+        String input = "report closed-lost by industry";
+        Assertions.assertEquals(Command.REPORT_CLOSED_LOST_BY_INDUSTRY, menu.checkCommand(input));
+    }
+
+    @Test
+    void checkCommand_reportOpenByIndustry_REPORT_OPEN_BY_INDUSTRY() {
+        String input = "report open by industry";
+        Assertions.assertEquals(Command.REPORT_OPEN_BY_INDUSTRY, menu.checkCommand(input));
+    }
+
+    @Test
+    void checkCommand_meanEmployeeCount_MEAN_EMPLOYEECOUNT() {
+        String input = "mean employeecount";
+        Assertions.assertEquals(Command.MEAN_EMPLOYEECOUNT, menu.checkCommand(input));
+    }
+
+    @Test
+    void checkCommand_medianEmployeeCount_MEDIAN_EMPLOYEECOUNT() {
+        String input = "median employeecount";
+        Assertions.assertEquals(Command.MEDIAN_EMPLOYEECOUNT, menu.checkCommand(input));
+    }
+
+    @Test
+    void checkCommand_maxEmployeeCount_MAX_EMPLOYEECOUNT() {
+        String input = "max employeecount";
+        Assertions.assertEquals(Command.MAX_EMPLOYEECOUNT, menu.checkCommand(input));
+    }
+
+    @Test
+    void checkCommand_minEmployeeCount_MIN_EMPLOYEECOUNT() {
+        String input = "min employeecount";
+        Assertions.assertEquals(Command.MIN_EMPLOYEECOUNT, menu.checkCommand(input));
+    }
+
+    @Test
+    void checkCommand_meanQuantity_MEAN_QUANTITY() {
+        String input = "mean quantity";
+        Assertions.assertEquals(Command.MEAN_QUANTITY, menu.checkCommand(input));
+    }
+
+    @Test
+    void checkCommand_medianQuantity_MEDIAN_QUANTITY() {
+        String input = "median quantity";
+        Assertions.assertEquals(Command.MEDIAN_QUANTITY, menu.checkCommand(input));
+    }
+
+    @Test
+    void checkCommand_maxQuantity_MAX_QUANTITY() {
+        String input = "max quantity";
+        Assertions.assertEquals(Command.MAX_QUANTITY, menu.checkCommand(input));
+    }
+
+    @Test
+    void checkCommand_minQuantity_MIN_QUANTITY() {
+        String input = "min quantity";
+        Assertions.assertEquals(Command.MIN_QUANTITY, menu.checkCommand(input));
+    }
+
+    @Test
+    void checkCommand_meanOppsPerAccount_MEAN_OPPS_PER_ACCOUNT() {
+        String input = "mean opps per account";
+        Assertions.assertEquals(Command.MEAN_OPPS_PER_ACCOUNT, menu.checkCommand(input));
+    }
+
+    @Test
+    void checkCommand_medianOppsPerAccount_MEDIAN_OPPS_PER_ACCOUNT() {
+        String input = "median opps per account";
+        Assertions.assertEquals(Command.MEDIAN_OPPS_PER_ACCOUNT, menu.checkCommand(input));
+    }
+
+    @Test
+    void checkCommand_maxOppsPerAccount_MAX_OPPS_PER_ACCOUNT() {
+        String input = "max opps per account";
+        Assertions.assertEquals(Command.MAX_OPPS_PER_ACCOUNT, menu.checkCommand(input));
+    }
+
+    @Test
+    void checkCommand_minOppsPerAccount_MIN_OPPS_PER_ACCOUNT() {
+        String input = "min opps per account";
+        Assertions.assertEquals(Command.MIN_OPPS_PER_ACCOUNT, menu.checkCommand(input));
+    }
+
+    @Test
+    void checkCommand_runScript_RUN() {
+        String input = "run script_test.crm";
+        Assertions.assertEquals(Command.RUN, menu.checkCommand(input));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
