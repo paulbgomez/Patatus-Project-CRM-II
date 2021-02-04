8f1d029d9085a948c5dd52423afb87cb25f2fd87
Paul Barroso <paulbg1993@gmail.com>
2021-02-04 22:09:49 +0100

    Tests finalizados opportunities

--- src/main/java/com/patatus/crmparte2/repository/OpportunityRepository.java
+++ src/main/java/com/patatus/crmparte2/repository/OpportunityRepository.java
@@ -12,6 +12,13 @@ import java.util.List;
 public interface OpportunityRepository extends JpaRepository<Opportunity, Integer> {
 
     /*
+    Order to make the median
+     */
+    @Query ("SELECT o.quantity FROM Opportunity o ORDER BY o.quantity")
+    List<Object[]> orderOpportunities();
+
+
+    /*
     The max quanitity of products order can be displayed by typing "Max Quantity"
      */
     @Query(value="SELECT s.name, o.quantity FROM opportunity o INNER JOIN sales_rep s ON o.rep_opportunity_id = s.id ORDER BY o.quantity DESC LIMIT 1", nativeQuery = true)
@@ -41,11 +48,6 @@ public interface OpportunityRepository extends JpaRepository<Opportunity, Intege
     @Query("SELECT AVG(o.quantity) FROM Opportunity o")
     double findAverageQuantityFromOpportunities();
 
-    /*
-   The median quanitity of products order can be displayed by typing "Mean Quantity"
-    */
-    @Query(value ="SELECT o.quantity Median FROM (SELECT o1.quantity, COUNT(o1.quantity) Rank FROM opportunity o1, opportunity o2 WHERE o1.quantity < o2.quantity OR (o1.quantity=o2.quantity) GROUP o1.quantity ORDER BY o1.quantity DESC) o3 WHERE Rank = (SELECT (COUNT(*)+1) DIV 2 FROM Opportunity)", nativeQuery = true)
-    Integer findMedianFromOpportunities();
 
     // A count of all Opportunities by product can be displayed by typing "Report Opportunity by product"
     @Query("SELECT o.product, COUNT(o) FROM Opportunity o GROUP BY o.product")
@@ -56,43 +58,36 @@ public interface OpportunityRepository extends JpaRepository<Opportunity, Intege
     // A count of all OPEN Opportunities by product can be displayed by typing "Report OPEN by product"
     // Instead of doing 3 separate methods, we have created one by which, passing it the required status,
     // gives you a report of all the opportunities that are with this status
-    // TODO HACER TEST
     @Query("SELECT o.product, COUNT(o.status) FROM Opportunity o WHERE o.status=:status GROUP BY o.product")
     List<Object[]> findByProductAndStatus(@Param("status") Enum status);
 
     // A count of all Opportunities by country can be displayed by typing "Report Opportunity by Country"
-    // TODO HACER TEST
     @Query("SELECT a.country, COUNT(o.id) FROM Opportunity o RIGHT JOIN o.account a GROUP BY a.country")
     List<Object[]> findOpportunitiesByCountry();
 
     // A count of all CLOSED_WON Opportunities by country can be displayed by typing "Report CLOSED-WON by Country"
     // A count of all CLOSED_LOST Opportunities by country can be displayed by typing "Report CLOSED-LOST by Country"
     // A count of all OPEN Opportunities by country can be displayed by typing "Report OPEN by Country"
-    // TODO HACER TEST
     @Query("SELECT a.country, COUNT(o.id) FROM Opportunity o RIGHT JOIN o.account a WHERE o.status=:status GROUP BY a.country")
     List<Object[]> findByCountryAndStatus(@Param("status") Enum status);
 
     // A count of all Opportunities by city can be displayed by typing "Report Opportunity by City"
-    // TODO HACER TEST
-    @Query("SELECT a.city, COUNT(o.id) FROM Opportunity o RIGHT JOIN o.account a GROUP BY a.country")
+    @Query("SELECT a.city, COUNT(o.id) FROM Opportunity o RIGHT JOIN o.account a GROUP BY a.city")
     List<Object[]> findOpportunitiesByCity();
 
     // A count of all CLOSED_WON Opportunities by city can be displayed by typing "Report CLOSED-WON by City"
     // A count of all CLOSED_LOST Opportunities by city can be displayed by typing "Report CLOSED-LOST by City"
     // A count of all OPEN Opportunities by city can be displayed by typing "Report OPEN by City"
-    // TODO HACER TEST
     @Query("SELECT a.city, COUNT(o.id) FROM Opportunity o RIGHT JOIN o.account a WHERE o.status=:status GROUP BY a.city")
     List<Object[]> findByCityAndStatus(@Param("status") Enum status);
 
     // A count of all Opportunities by industry can be displayed by typing "Report Opportunity by Industry"
-    // TODO HACER TEST
     @Query("SELECT a.industry, COUNT(o.id) FROM Opportunity o RIGHT JOIN o.account a GROUP BY a.industry")
     List<Object[]> findOpportunitiesByIndustry();
 
     // A count of all CLOSED_WON Opportunities by industry can be displayed by typing "Report CLOSED-WON by Industry"
     // A count of all CLOSED_LOST Opportunities by industry can be displayed by typing "Report CLOSED-LOST by Industry"
     // A count of all OPEN Opportunities by industry can be displayed by typing "Report OPEN by Industry"
-    // TODO HACER TEST
     @Query("SELECT a.industry, COUNT(o.id) FROM Opportunity o RIGHT JOIN o.account a WHERE o.status=:status GROUP BY a.industry")
     List<Object[]> findByIndustryAndStatus(@Param("status") Enum status);
 
--- src/test/java/com/patatus/crmparte2/repository/OpportunityRepositoryTest.java
+++ src/test/java/com/patatus/crmparte2/repository/OpportunityRepositoryTest.java
@@ -119,9 +119,85 @@ class OpportunityRepositoryTest {
     }
 
     @Test
-    public void findMedianFromOpportunities(){
-        Integer median = opportunityRepository.findMedianFromOpportunities();
-        assertEquals(70, median);
+    public void findAverage(){
+        assertEquals(60., opportunityRepository.findAverageQuantityFromOpportunities());
     }
+
+    @Test
+    public void orderOpportunities(){
+        List<Object[]>  objects = opportunityRepository.orderOpportunities();
+
+        if(objects.size() % 2 != 0 ){
+            Integer median = (objects.size()-1)/2;
+            System.out.println("Median is " + objects.get(median)[0]);
+        } else {
+            Integer firstHalf = (Integer) objects.get((objects.size()/2)-1)[0];
+            Integer secondHalf = (Integer) objects.get(objects.size()/2)[0];
+            System.out.println("Median is " + (firstHalf + secondHalf)/2);
+        }
+
+        assertEquals(5, objects.size());
+    }
+
+    @Test
+    public void findByProduct(){
+        List<Object[]> objects = opportunityRepository.findOpportunitiesByProduct();
+        assertEquals(2L, objects.size());
+        assertEquals(Product.BOX, objects.get(0)[0]);
+        assertEquals(Product.FLATBED, objects.get(1)[0]);
+    }
+
+    @Test
+    public void findByProductAndStatus(){
+        List<Object[]> objects = opportunityRepository.findByProductAndStatus(Status.OPEN);
+        assertEquals(2L, objects.size());
+    }
+
+    @Test
+    public void findOpportunitiesByCountry(){
+        List<Object[]> objects = opportunityRepository.findOpportunitiesByCountry();
+        assertEquals(accountRepository.findAll().get(0).getCountry(), objects.get(0)[0]);
+        assertEquals("USA", objects.get(0)[0]);
+    }
+
+    @Test
+    public void findByCountryAndStatus(){
+        List<Object[]> objects = opportunityRepository.findByCountryAndStatus(Status.OPEN);
+        assertEquals(accountRepository.findAll().get(0).getCountry(), objects.get(0)[0]);
+        assertEquals(1, objects.size());
+        assertEquals(5L, objects.get(0)[1]);
+    }
+
+    @Test
+    public void findOpportunitiesByCity(){
+        List<Object[]> objects = opportunityRepository.findOpportunitiesByCity();
+        assertEquals(accountRepository.findAll().get(0).getCity(), objects.get(0)[0]);
+        assertEquals("Detroit", objects.get(0)[0]);
+    }
+
+    @Test
+    public void findByCityAndStatus(){
+        List<Object[]> objects = opportunityRepository.findByCityAndStatus(Status.OPEN);
+        assertEquals(accountRepository.findAll().get(0).getCity(), objects.get(0)[0]);
+        assertEquals(1, objects.size());
+        assertEquals(5L, objects.get(0)[1]);
+    }
+
+    @Test
+    public void findOpportunitiesByIndustry(){
+        List<Object[]> objects = opportunityRepository.findOpportunitiesByIndustry();
+        assertEquals(accountRepository.findAll().get(0).getCity(), objects.get(0)[0]);
+        assertEquals(Industry.ECOMMERCE, objects.get(0)[0]);
+    }
+
+    @Test
+    public void findOpportunitiesByIndustryandStatus(){
+        List<Object[]> objects = opportunityRepository.findByIndustryAndStatus(Status.OPEN);
+        assertEquals(accountRepository.findAll().get(0).getIndustry(), objects.get(0)[0]);
+        assertEquals(1, objects.size());
+        assertEquals(5L, objects.get(0)[1]);
+    }
+
+
 
 }
