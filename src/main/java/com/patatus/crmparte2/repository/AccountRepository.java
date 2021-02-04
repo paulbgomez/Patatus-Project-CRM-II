43a2cab41295d70c3b2194e5ce2288079ea627af
Paul Barroso <paulbg1993@gmail.com>
2021-02-04 23:49:26 +0100

    Account tests

--- src/main/java/com/patatus/crmparte2/repository/AccountRepository.java
+++ src/main/java/com/patatus/crmparte2/repository/AccountRepository.java
@@ -9,20 +9,36 @@ import java.util.List;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Integer> {
-    // 1. The mean employeeCount can be displayed by typing "Mean EmployeeCount"
+    // The mean employeeCount
     @Query("SELECT AVG(employeeCount) FROM Account")
     public Double findMeanEmployeeCount();
 
     // 2. The median employeeCount can be displayed by typing "Median EmployeeCount"
-    // TODO ¿Quizá me traigo los valores a pelo y lo hago con java? :'D
 
-    // 3. The maximum employeeCount can be displayed by typing "Max EmployeeCount"
+    // The maximum employeeCount
     @Query(value="SELECT a.id, a.employee_count FROM account a ORDER BY a.employee_count DESC LIMIT 1", nativeQuery = true)
     public List<Object[]> findMaxEmployeeCount();
 
-    // 4. The minimum employeeCount can be displayed by typing "Min EmployeeCount"
+    // The minimum employeeCount
     @Query(value="SELECT a.id, a.employee_count FROM account a ORDER BY a.employee_count ASC LIMIT 1", nativeQuery = true)
     public List<Object[]> findMinEmployeeCount();
+
+    // Ordered opportunities by account for the median
+    @Query(value="SELECT oo.count FROM (SELECT COUNT(o.id) AS count FROM account a LEFT JOIN opportunity o ON a.id = o.id GROUP BY a.id) AS oo ORDER BY count", nativeQuery = true)
+    public List<Object[]> findOpportunitiesByAccountOrdered();
+
+    // Max opportunities by account for the median
+    @Query(value="SELECT oo.count FROM (SELECT COUNT(o.id) AS count FROM account a LEFT JOIN opportunity o ON a.id = o.id GROUP BY a.id) AS oo ORDER BY count DESC LIMIT 1", nativeQuery = true)
+    public List<Object[]> findMaxOpportunitiesByAccount();
+
+    // Min opportunities by account for the median
+    @Query(value="SELECT oo.count FROM (SELECT COUNT(o.id) AS count FROM account a LEFT JOIN opportunity o ON a.id = o.id GROUP BY a.id) AS oo ORDER BY count ASC LIMIT 1", nativeQuery = true)
+    public List<Object[]> findMinOpportunitiesByAccount();
+
+    // Average opportunities by account for the median
+    @Query(value="SELECT AVG(oo.count) FROM (SELECT COUNT(o.id) AS count FROM account a LEFT JOIN opportunity o ON a.id = o.id GROUP BY a.id) AS oo", nativeQuery = true)
+    public double findAvgOpportunitiesByAccount();
+
 
 
 }
