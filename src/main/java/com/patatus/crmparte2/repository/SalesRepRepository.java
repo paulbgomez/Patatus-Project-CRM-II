ee763071e899d35dd126e334215ac0966e23b321
coby-42 <77333935+coby-42@users.noreply.github.com>
2021-02-05 12:08:23 +0100

    fixed exists salesRep, ahora sí

--- src/main/java/com/patatus/crmparte2/repository/SalesRepRepository.java
+++ src/main/java/com/patatus/crmparte2/repository/SalesRepRepository.java
@@ -11,18 +11,22 @@ import java.util.List;
 @Repository
 public interface SalesRepRepository extends JpaRepository<SalesRep, Integer> {
     //A count of Leads by SalesRep
-        @Query("SELECT s.name,COUNT(l) FROM Lead l INNER JOIN l.repLead s GROUP BY s")
-        public List<Object[]> findLeadCountBySalesRep();
+    @Query("SELECT s.name,COUNT(l) FROM Lead l INNER JOIN l.repLead s GROUP BY s")
+    List<Object[]> findLeadCountBySalesRep();
 
-//    @Query(value="SELECT s.name,COUNT(*) FROM leads l INNER JOIN sales_rep s ON l.rep_lead_id=s.id  GROUP BY s.name",nativeQuery = true)
-//    public List<Object[]> findLeadCountBySalesRep();
+    //@Query(value="SELECT s.name,COUNT(*) FROM leads l INNER JOIN sales_rep s ON l.rep_lead_id=s.id  GROUP BY s.name",nativeQuery = true)
+    //public List<Object[]> findLeadCountBySalesRep();
 
     //A count of all Opportunities by SalesRep
-        @Query("SELECT s.name,COUNT(o) FROM SalesRep s LEFT JOIN s.repOpportunity o GROUP BY s")
-        public List<Object[]> findOpportunityCountBySalesRep();
+    @Query("SELECT s.name,COUNT(o) FROM SalesRep s LEFT JOIN s.repOpportunity o GROUP BY s")
+    List<Object[]> findOpportunityCountBySalesRep();
 
 
     //A count of Opportunities by Status and Sales Rep
-        @Query("SELECT s.name,COUNT(o) FROM SalesRep s LEFT JOIN s.repOpportunity o WHERE o.status = :status GROUP BY s")
-        public List<Object[]> findOpportunityByStatusCountBySalesRep(@Param("status")Enum status);
+    @Query("SELECT s.name,COUNT(o) FROM SalesRep s LEFT JOIN s.repOpportunity o WHERE o.status = :status GROUP BY s")
+    List<Object[]> findOpportunityByStatusCountBySalesRep(@Param("status") Enum status);
+
+    @Query("SELECT COUNT(id) FROM Account")
+    Integer countSalesRep();
+
 }
